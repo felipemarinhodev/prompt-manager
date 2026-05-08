57da22bf1605f2d112afc4657ca0c8c6e3d80636
Felipe Marinho <github@felipemarinho.dev>
2026-05-08 06:48:07 -0700

    feat: enhance Jest setup with global TextEncoder, TextDecoder, and webcrypto support

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import '@testing-library/jest-dom';
+
+import { webcrypto } from 'node:crypto';
+import { TextEncoder, TextDecoder } from 'node:util';
+
+(globalThis as any).TextEncoder = TextEncoder;
+(globalThis as any).TextDecoder = TextDecoder;
+if (!(globalThis as any).crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
 
 expect.extend({});
