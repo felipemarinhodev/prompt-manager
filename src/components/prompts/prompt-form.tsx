c4b2eee1460bfa4f13686fba4c7bf26d67435db7
Felipe Marinho <github@felipemarinho.dev>
2026-05-10 11:04:45 -0700

    feat: add PromptForm component with input and textarea fields

---
+++ src/components/prompts/prompt-form.tsx
@@ -0,0 +1,28 @@
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+
+export const PromptForm = () => {
+  return (
+    <form className="space-y-6">
+      <header className="flex flex-wrap gap-2 items-center mb-6 justify-end">
+        <Button type="submit" size="sm">
+          Salvar
+        </Button>
+      </header>
+
+      <Input
+        placeholder="Título do prompt"
+        variant="transparent"
+        size="lg"
+        autoFocus
+      />
+
+      <Textarea
+        placeholder="Digite o conteúdo do prompt..."
+        variant="transparent"
+        size="lg"
+      />
+    </form>
+  );
+};
