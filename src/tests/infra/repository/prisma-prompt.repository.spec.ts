ab83e2889181e1985e32cab7e5e73cafcd1d324a
Felipe Marinho <github@felipemarinho.dev>
2026-05-09 17:09:01 -0700

    feat: add tests for PrismaPromptRepository to validate findMany and searchMany methods

---
+++ src/tests/infra/repository/prisma-prompt.repository.spec.ts
@@ -0,0 +1,123 @@
+import { Prompt } from '@/core/domain/prompts/prompt.entity';
+import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPromptRepository } from '@/infra/repository/prisma-prompt.repository';
+
+type PromptDelegateMock = {
+  findMany: jest.MockedFunction<
+    (args: {
+      where?: {
+        OR: [
+          { title?: { contains: string; mode: 'insensitive' } },
+          { content?: { contains: string; mode: 'insensitive' } },
+        ];
+      };
+      orderBy?: { createdAt: 'asc' | 'desc' };
+    }) => Promise<Prompt[]>
+  >;
+};
+
+type PrismaMock = {
+  prompt: PromptDelegateMock;
+};
+
+function createMockPrisma() {
+  const mock: PrismaMock = {
+    prompt: {
+      findMany: jest.fn(),
+    },
+  };
+  return mock as unknown as PrismaClient & PrismaMock;
+}
+
+describe('PrismaPromptRepository', () => {
+  let prisma: ReturnType<typeof createMockPrisma>;
+  let repository: PrismaPromptRepository;
+
+  beforeEach(() => {
+    prisma = createMockPrisma();
+    repository = new PrismaPromptRepository(prisma);
+  });
+
+  describe('findMany', () => {
+    it('should order by createdAt in descending order and map the results correctly', async () => {
+      const now = new Date();
+
+      const input = [
+        {
+          id: '1',
+          title: 'Prompt 1',
+          content: 'Content 1',
+          createdAt: now,
+          updatedAt: now,
+        },
+        {
+          id: '2',
+          title: 'Prompt 2',
+          content: 'Content 2',
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+      prisma.prompt.findMany.mockResolvedValue(input);
+
+      const results = await repository.findMany();
+
+      expect(prisma.prompt.findMany).toHaveBeenCalledWith({
+        orderBy: { createdAt: 'desc' },
+      });
+      expect(results).toEqual(input);
+    });
+  });
+
+  describe('searchMany', () => {
+    it('should search by empty term and not sent the where clause', async () => {
+      const now = new Date();
+
+      const input = [
+        {
+          id: '1',
+          title: 'Prompt 1',
+          content: 'Content 1',
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+      prisma.prompt.findMany.mockResolvedValue(input);
+
+      const results = await repository.searchMany('       ');
+
+      expect(prisma.prompt.findMany).toHaveBeenCalledWith({
+        where: undefined,
+        orderBy: { createdAt: 'desc' },
+      });
+      expect(results).toMatchObject(input);
+    });
+    it('should search by term and set where clause', async () => {
+      const now = new Date();
+
+      const input = [
+        {
+          id: '1',
+          title: 'Prompt 1',
+          content: 'Content 1',
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+      prisma.prompt.findMany.mockResolvedValue(input);
+
+      const results = await repository.searchMany('   prompt 1    ');
+
+      expect(prisma.prompt.findMany).toHaveBeenCalledWith({
+        where: {
+          OR: [
+            { title: { contains: 'prompt 1', mode: 'insensitive' } },
+            { content: { contains: 'prompt 1', mode: 'insensitive' } },
+          ],
+        },
+        orderBy: { createdAt: 'desc' },
+      });
+      expect(results).toMatchObject(input);
+    });
+  });
+});
