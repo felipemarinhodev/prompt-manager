b3c31d959227e383a7a7f80378d62142e5f4c9c8
Felipe Marinho <github@felipemarinho.dev>
2026-05-10 17:53:32 -0700

    feat: implement form handling for prompt creation using react-hook-form

--- src/components/prompts/prompt-form.tsx
+++ src/components/prompts/prompt-form.tsx
@@ -1,8 +1,24 @@
+'use client';
+
+import { Controller, useForm } from 'react-hook-form';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Textarea } from '../ui/textarea';
+import {
+  CreatePromptDTO,
+  createPromptSchema,
+} from '@/core/application/prompts/create-prompt.dto';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export const PromptForm = () => {
+  const form = useForm<CreatePromptDTO>({
+    resolver: zodResolver(createPromptSchema),
+    defaultValues: {
+      title: '',
+      content: '',
+    },
+  });
+
   return (
     <form className="space-y-6">
       <header className="flex flex-wrap gap-2 items-center mb-6 justify-end">
@@ -10,18 +26,31 @@ export const PromptForm = () => {
           Salvar
         </Button>
       </header>
-
-      <Input
-        placeholder="Título do prompt"
-        variant="transparent"
-        size="lg"
-        autoFocus
+      <Controller
+        name="title"
+        control={form.control}
+        render={({ field }) => (
+          <Input
+            {...field}
+            placeholder="Título do prompt"
+            variant="transparent"
+            size="lg"
+            autoFocus
+          />
+        )}
       />
 
-      <Textarea
-        placeholder="Digite o conteúdo do prompt..."
-        variant="transparent"
-        size="lg"
+      <Controller
+        name="content"
+        control={form.control}
+        render={({ field }) => (
+          <Textarea
+            {...field}
+            placeholder="Digite o conteúdo do prompt..."
+            variant="transparent"
+            size="lg"
+          />
+        )}
       />
     </form>
   );
