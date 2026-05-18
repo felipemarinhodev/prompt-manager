fb5cae7e948c60a670099af1d2507f499e2c55cf
Felipe Marinho <github@felipemarinho.dev>
2026-05-17 19:39:46 -0700

    test: add end-to-end test for handling duplicate prompt titles

--- e2e/prompt-create.spec.ts
+++ e2e/prompt-create.spec.ts
@@ -1,4 +1,7 @@
+import { Prisma, PrismaClient } from '@/generated/prisma/client';
 import test, { expect } from '@playwright/test';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 test('should create a new prompt successfully', async ({ page }) => {
   const uniqueTitle = `E2E Prompt ${Date.now()}`;
@@ -16,4 +19,33 @@ test('should create a new prompt successfully', async ({ page }) => {
     state: 'visible',
     timeout: 15000,
   });
+});
+
+test('should show error message when prompt create duplicate title', async ({
+  page,
+}) => {
+  const duplicateTitle = 'Duplicate Title';
+  const content = `E2E Content ${Date.now()}`;
+
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+  const adapter = new PrismaPg(pool);
+  const prisma = new PrismaClient({ adapter });
+  await prisma.prompt.deleteMany({ where: { title: duplicateTitle } });
+  await prisma.prompt.create({ data: { title: duplicateTitle, content } });
+  await prisma.$disconnect();
+
+  // Now, try to create another prompt with the same title
+  await page.goto('/new');
+  await page.fill('input[name="title"]', duplicateTitle);
+  await page.fill('textarea[name="content"]', content);
+  await page.getByRole('button', { name: /salvar/i }).click();
+
+  await page.waitForSelector('text=Este prompt já existe', {
+    state: 'visible',
+    timeout: 15000,
+  });
+  await expect(page.getByRole('heading', { name: duplicateTitle })).toHaveCount(
+    1
+  );
 });
